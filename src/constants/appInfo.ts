0208e49fcd02e4b56c544bafc43592a4379a427c
DRAKKOSHKKA <drakkoshkka@internet.ru>
2026-09-20 00:44:55 +0300

    release: bump version to v1.B4 with new 3D app icon and splash screen

--- src/constants/appInfo.ts
+++ src/constants/appInfo.ts
@@ -5,8 +5,8 @@
  */
 export const APP_CONFIG = {
 	name: "Apkify",
-	version: "1.B3",
-	buildNumber: "3",
+	version: "1.B4",
+	buildNumber: "4",
 	collegeName: "Альметьевский профессиональный колледж",
 	ownerId: 37,
 	websiteUrl: "https://it-institut.ru/SearchString/Index/37",
